fc0f5eda94299ce6688d4849b3b6009bcd88837a
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2017-10-04 18:45:33 +0200

    Workarounds invalid wiki urls. Minor improvements. Updated README
    Problematics wikis:
    * 1161 throws 404 error when fetching its main page
    * 1221 throws error when requesting (http head or http get) $wiki_url . 'Special:ListUsers'

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
-# dl_xml
+# wikia_dump_downloader
 Download the current revisions of a repository from a Mediawiki site via Special:Export and the API.
 
-## Dependencies
+# census
+
+Generate a 'census' or corpus of wikia hosted wikis, based on number of users, editions, pages, etcetera.
+
+TODO: Generate alternative census of 'Deleted' wikias.
+
+# Dependencies
 * perl 5
 * Bundle::LWP. In debian and derivatives it's included in the `libwww-perl` package. Alternatively, It can be installed using CPAN: `perl -MCPAN -e 'install Bundle::LWP'`
--- get_wikia_census.pl
+++ get_wikia_census.pl
@@ -15,12 +15,12 @@ my $br = LWP::UserAgent->new;
 $br->timeout(15);
 $br->conn_cache(LWP::ConnCache->new());
 $br->agent("Mozilla/5.0");
-$br->requests_redirectable(['POST',]); # Only automatically redirects for POST requests
+$br->requests_redirectable(['POST', 'HEAD', 'GET']);
 
 
 # Define id max to iterate until.
-my $WIKIA_ID_INIT = 1;
-my $WIKIA_ID_MAX = 550;
+my $WIKIA_ID_INIT = 1161;
+my $WIKIA_ID_MAX = 1161;
 
 # wikia API
 my $wikia_endpoint = 'http://www.wikia.com/api/v1';
@@ -64,7 +64,7 @@ sub extract_wiki_info_from_wikia_json {
         $wiki_hub = $wiki_info->{'hub'};
         $wiki_topic = $wiki_info->{'topic'};
         $wiki_lang = $wiki_info->{'lang'};
-        
+
         my $wiki_stats = $wiki_info->{'stats'};
         $wiki_edits = $wiki_stats->{'edits'};
         $wiki_articles = $wiki_stats->{'articles'};
@@ -92,7 +92,7 @@ sub request_all_users {
         if (not $res->is_success) {
                 die $res->status_line.' when posting to Special:ListUsers for edits ' . $edits;
         }
-        
+
         my $raw_users_content = $res->decoded_content();
         my $json_res = decode_json($raw_users_content);
         #~ print Dumper($json_res);
@@ -132,6 +132,8 @@ sub extract_users_by_contributions {
         my $no_bot_users;
         my $edits_query;
         my $loop_query;
+
+        # Do different queries to get contributors by nº of editions made.
         for (my $i = 0; $i < 6; ++$i) {
                 if ($i == 0) {$edits_query = 1;}
                 elsif ($i == 1) {$edits_query = 5; sleep 1;}
@@ -139,12 +141,12 @@ sub extract_users_by_contributions {
                 elsif ($i == 3) {$edits_query = 20; sleep 1;}
                 elsif ($i == 4) {$edits_query = 50;}
                 else {$edits_query = 100; sleep 1;} # ($i == 5)
-                
+
                 $loop_query = $i * 2 + 1;
                 $no_bot_users = request_all_users($loop_query, $edits_query) - request_bot_users($loop_query + 1, $edits_query);
                 say "Non-bot users with edits equal or higher than $edits_query is: $no_bot_users";
                 $users_by_contributions[$i] = $no_bot_users;
-        }        
+        }
 }
 
 
@@ -154,34 +156,40 @@ sub print_wiki_to_csv {
         print CSV "$wikia_id, $wiki_name, $wiki_url, $wiki_pages, $wiki_active_users, $wiki_admins, $users_by_contributions[0], $users_by_contributions[1], $users_by_contributions[2], $users_by_contributions[3], $users_by_contributions[4], $users_by_contributions[5], $wiki_edits, $wiki_lang, $wiki_hub, $wiki_topic \n";
 }
 
+# returns: 1 if url is ok, -1 if the wiki's been deleted or 0 in case of an http error.
 sub is_wiki_url_ok {
-        my $res = $br->get($wiki_url . $listUsers_post_endpoint);
-        
-        # To inspect past redirects if automatic redirect was enabled:
-        #my @redirects = $res->redirects();
-        #foreach my $r (@redirects) {
-                #my $req = $r->request();
-                #print($req->as_string());
-                #print($r->as_string());
-        #}
-        
-        if ($res->is_success) {
-                return 1;
-        } elsif ($res->is_redirect) {
-                my $wiki_url_new = $res->header('Location');
-                say "Changing wiki url from $wiki_url to $wiki_url_new by an HTTP redirect";
-                if ($wiki_url_new =~ m/community\.wikia\.com\/wiki\/Special\:CloseWiki/) {
-                        return 0; # return "wiki is deleted"
-                } else {
-                        say "Executing again is_wiki_url_ok following redirect to $wiki_url_new";
-                        $wiki_url = $wiki_url_new;
-                        return is_wiki_url_ok();
+        my $res = $br->head($wiki_url);
+
+        my @redirects = $res->redirects();
+        foreach my $redirect (@redirects) {
+
+                if (($redirect->as_string()) =~ m/community\.wikia\.com\/wiki\/Special\:CloseWiki/) {
+                        print "\n--- Wiki $wiki_name with id $wikia_id has been deleted from wikia ---\n";
+                        return -1; # return "wiki has been deleted"
                 }
-        } else {
-                die "Unexpected error, checking wiki url $wiki_url for wiki with id $wikia_id: " . $res->status_line;
         }
-        
-        return 1;
+
+        if ($res->is_success) {
+                return 1; # return "wiki url is ok"
+        #~ } elsif ($res->is_redirect) {
+                #~ my $wiki_url_new = uri_unescape($res->header('Location'));
+                #~ my $wiki_url_new = $res->header('Location');
+
+                #~ print $wiki_url_new;
+                #~ die;
+
+                #~ say "Changing wiki url from $wiki_url to $wiki_url_new by an HTTP redirect";
+                #~ if ($wiki_url_new =~ m/community\.wikia\.com\/wiki\/Special\:CloseWiki/) {
+                        #~ return -1; # return "wiki has been deleted"
+                #~ } else {
+                        #~ say "Executing again is_wiki_url_ok with new URL.\n";
+                        #~ $wiki_url = $wiki_url_new;
+                        #~ return is_wiki_url_ok();
+                #~ }
+        } else {
+                say STDERR "Error found checking wiki $wiki_name with url $wiki_url for wiki with id $wikia_id: " . $res->status_line;
+                return 0; # return "There's an error requesting $wiki_url"
+        }
 }
 
 
@@ -195,29 +203,29 @@ print CSV "$csv_columns\n";
 # Iterating over ids
 my $retried = 0; # var to mark if I already retried a 500 responded request.
 for ($wikia_id = $WIKIA_ID_INIT; $wikia_id <= $WIKIA_ID_MAX; $wikia_id++) {
-        
-        print "\n\n";        
+
+        print "\n\n";
         say('#' x 30);
         print "\n\n";
-        
+
         say ("Retrieving data for wiki with id: $wikia_id");
-        
+
         sleep 1; # Artificial delay to not saturate wikia' servers
-        
+
         # Getting wiki's canonical url from wikia general API
         my $wikia_params = '/Wikis/Details?ids=' . $wikia_id; # params targetting wikia api to get wiki info
         my $api_request = $wikia_endpoint . $wikia_params;
         my $res = $br->get($api_request);
-        
+
         if (not $res->is_success) {
                 # In case of a 500 response, wait for a moment and retry again.
                 if ($res->code == HTTP_INTERNAL_SERVER_ERROR && $retried < 3) {
-                        say "Received 500 Internal Server Error response. Retrying again after 10 seconds";
+                        say STDERR "Received 500 Internal Server Error response. Retrying again after 10 seconds...";
                         sleep 10;
                         $retried++;
                         redo;
                 } else {
-                        die $res->status_line . ' when getting wikias from ' . $api_request;
+                        die 'Too many intents: ' . $res->status_line . ' when getting wikias from ' . $api_request;
                 }
         }
         my $json_res = decode_json($res->decoded_content);
@@ -230,25 +238,28 @@ for ($wikia_id = $WIKIA_ID_INIT; $wikia_id <= $WIKIA_ID_MAX; $wikia_id++) {
         # Getting general info using wikia API:
         $wiki_info = $json_res->{'items'}->{$wikia_id};
         extract_wiki_info_from_wikia_json();
-        
-        my $deleted = not is_wiki_url_ok();
-        if ($deleted) {
-                print "\n--- Wiki with id $wikia_id has been deleted from wikia ---\n";
+
+        my $wiki_url_status = is_wiki_url_ok();
+        if ($wiki_url_status == -1 ) {
+                #TODO: print "Storing it in deleted_wikis.csv \n"
+                next;
+        } elsif ($wiki_url_status == 0) {
+                # http error found. Skipping from census.
                 next;
         }
-        
+
         # Getting users using Special:ListUsers page
         $listUsers_url = $wiki_url . $listUsers_post_endpoint;
         extract_users_by_contributions();
-        
-        
+
+
         # Info retrieved, printing:
         print_wiki_to_csv();
-        
+
         say ("Non bot users per contribution: ");
         print "$_, " foreach (@users_by_contributions);
         print "\n";
-        
+
         $retried = 0; # clean up retried
 }
 

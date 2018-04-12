d2411ef7c43f3f03b9a9dad8f3983f3976a758b8
Abel Serrano Juste <akronix5@gmail.com>
2018-04-12 17:29:37 +0200

    Changed spaces for tabs in shell scripts

--- wikia_dump_downloader/join_xml_parts.sh
+++ wikia_dump_downloader/join_xml_parts.sh
@@ -9,12 +9,12 @@ prefix=$1
 ## Variables definition and directories creation ##
 # Setting a directory to place output based on prefix (script argument)
 if [ -z $prefix ]; then
-  xml_list=$(ls -1b ${data_dir}/*.xml)
-  prefix='output'
-  output_xml="$processed_dir/$prefix/full/output.xml"
+	xml_list=$(ls -1b ${data_dir}/*.xml)
+	oprefix='output'
+	output_xml="$processed_dir/$prefix/full/output.xml"
 else
-  xml_list=$(ls -1b ${data_dir}/${prefix}*.xml)
-  output_xml="$processed_dir/$prefix/full/$prefix.xml"
+	xml_list=$(ls -1b ${data_dir}/${prefix}*.xml)
+	output_xml="$processed_dir/$prefix/full/$prefix.xml"
 fi
 
 
@@ -34,21 +34,21 @@ xml_no=`echo $xml_list | wc -w`
 echo "Number of parts to join: $(($xml_no))"
 
 for file in $xml_list; do
-        echo "Processing file $file"
-        `cp $file aux.xml`
-        if [ $i -ne 0 ]; then
-                sed '1d' aux.xml > tmp.xml; # deleting first line
-                sed '/<siteinfo>/,/<\/siteinfo>/d' tmp.xml > aux.xml # deleting <siteinfo> element
-        fi
-        if [ $i -ne $(($xml_no-1)) ]; then
-                sed '$d' aux.xml > tmp.xml; # deleting last line
-                mv tmp.xml aux.xml;
-        fi
-        
-        cat aux.xml >> $output_xml;
-        i=$(($i+1));
-        filename=`echo $file | cut -d '/' -f 2`
-        mv $file "$processed_dir/$prefix/parts/$filename"
+	echo "Processing file $file"
+	`cp $file aux.xml`
+	if [ $i -ne 0 ]; then
+		sed '1d' aux.xml > tmp.xml; # deleting first line
+		sed '/<siteinfo>/,/<\/siteinfo>/d' tmp.xml > aux.xml # deleting <siteinfo> element
+	fi
+	if [ $i -ne $(($xml_no-1)) ]; then
+		sed '$d' aux.xml > tmp.xml; # deleting last line
+		mv tmp.xml aux.xml;
+	fi
+
+	cat aux.xml >> $output_xml;
+	i=$(($i+1));
+	filename=`echo $file | cut -d '/' -f 2`
+	mv $file "$processed_dir/$prefix/parts/$filename"
 done
 
 # Cleaning temporary files

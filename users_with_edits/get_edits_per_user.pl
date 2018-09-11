Grasia/wiki-scripts

commit bff10a446414603b7be3805c311a82e32e0324a1

Don't print trailing commas for the lists of user edits. Use "curated" index by default as filename input.

users_with_edits/get_edits_per_user.pl
@@ -27,7 +27,7 @@ my $listUsers_post_endpoint = 'index.php?' . 'action=ajax&rs=ListusersAjax::axSh
 
 # file-related variables
 my $output_filename = 'wikia_edits-partk.csv';
-my $urls_filename = '20180220-wikiaIndex-partk.txt';
+my $urls_filename = '20180220-wikiaCuratedIndex-partk.txt';
 my $csv_columns = 'url; wiki_name; total_edits; edits_per_user; bots';
 
 # output filehandlers
@@ -117,6 +117,8 @@ sub print_all_users {
     my $data = $json_res->{'aaData'};
 
     my @user_edits = @$data;
+    my $first_element = 1;
+
     foreach (@user_edits) {
         # filter out bots in case we aren't querying bots:
         if (not $query_bots and @$_[1] =~ /bot/i) {
@@ -125,12 +127,22 @@ sub print_all_users {
             next;
         }
 
+
         $dirty_edits = @$_[2];
         my $edits = $hs->parse( $dirty_edits );
         $hs->eof;
-        print ("$edits, ");
-        print $output_fh ("$edits, ");
+        # First element of @user_edits without a trailing comma:
+        if ($first_element) {
+            print ("$edits");
+            print $output_fh ("$edits");
+            $first_element = 0;
+        } else {
+            print (", $edits");
+            print $output_fh (", $edits");
+        }
     }
+
+
 
     return 0;
 

commit 3c97dcd3b04830842248d7849c430024c0bb23b8

Fixes a bug in bff10a446414603b7be3805c311a82e32e0324a1 which caused to not print commas after every batch of edit data.

users_with_edits/get_edits_per_user.pl
@@ -52,9 +52,12 @@ sub open_output_file {
 }
 
 
-# order of arguments = ($loop, $url, $query_bots)
+# order of arguments = ($loop, $url, $query_bots, $first_element)
+# first_elemnt is just to know if this is the first time this subroutine is
+# executed for a wiki, because in that case we don't want to print a comma
+# before the number
 sub print_all_users {
-    my ($loop, $url, $query_bots) = @_;
+    my ($loop, $url, $query_bots, $first_element) = @_;
     $offset = $limit * $loop;
     my @form_data ;
     if (not $query_bots) {
@@ -117,7 +120,6 @@ sub print_all_users {
     my $data = $json_res->{'aaData'};
 
     my @user_edits = @$data;
-    my $first_element = 1;
 
     foreach (@user_edits) {
         # filter out bots in case we aren't querying bots:
@@ -159,17 +161,22 @@ sub print_all_users {
 sub extract_edits_and_print {
     my ($url) = @_;
     my $loop;
+    my $first_element; # to do not print comma before the first element.
+
 
     # printing edits per human user using Special:ListUsers page
     $loop = 0;
+    $first_element = 1;
+
     do {
-        if (print_all_users($loop, $url, 0) < 0) {
+        if (print_all_users($loop, $url, 0, $first_element) < 0) {
             print STDERR $res->status_line.' when posting to Special:ListUsers querying for all users.\n';
             print STDERR "--> Skipping from index <-- \n";
             print $output_fh ("-1; -1\n");
             return -1;
         }
         $loop++;
+        $first_element = 0;
     } while ($loop <= $users / $limit);
 
     print ("; ");
@@ -177,14 +184,17 @@ sub extract_edits_and_print {
 
     # printing edits per bot user using Special:ListUsers page
     $loop = 0;
+    $first_element = 1;
+
     do {
-        if (print_all_users($loop, $url, 1) < 0) {
+        if (print_all_users($loop, $url, 1, $first_element) < 0) {
             print STDERR $res->status_line.' when posting to Special:ListUsers querying for all users.\n';
             print STDERR "--> Skipping from index <-- \n";
             print $output_fh ("-1\n");
             return -1;
         }
         $loop++;
+        $first_element = 0;
     } while ($loop <= $users / $limit);
 
 
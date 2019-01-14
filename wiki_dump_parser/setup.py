bf96b3a297f8b30c42ee24de2ca50f17d68ee34c
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2019-01-14 16:16:43 +0100

    Added README. Closes #11

--- wiki_dump_parser/setup.py
+++ wiki_dump_parser/setup.py
@@ -1,17 +1,26 @@
 from setuptools import setup
+from os import path
 
 __version__ = '1.0.0'
+
+
+# read the contents of the README file
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='wiki_dump_parser',
     author="Abel 'Akronix' Serrano Juste",
     author_email='akronix5@gmail.com',
-    description='A simple but fast python script that reads a XML dump of a \
-wiki and output the processed data in a CSV file.',
+    description='A simple but fast python script that reads the XML dump of a \
+    wiki and output the processed data in a CSV file.',
     license="AGPL-3.0",
     keywords="wiki dump parser Wikia xml csv pandas proccessing history data",
     url='https://github.com/Grasia/wiki-scripts/tree/master/wiki_dump_parser',
     version=__version__,
     py_modules=['wiki_dump_parser'],
     python_requires='>=3',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )

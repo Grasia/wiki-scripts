c9f5c83655b98971801330b49d717a2048032fa5
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2019-01-18 15:25:18 +0000

    Added better error control for bulk_download.sh

--- wiki_dump_downloader/bulk_download.sh
+++ wiki_dump_downloader/bulk_download.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 # shell script to download the wiki dump for every wiki listed in a wiki index
-#  file (first argument) of a list of wikis
+#  file (first argument) of a list of wikis domain names
 #  and join all the parts into one only XML file.
+# IMPORTANT: DO NOT INCLUDE PRECEEDING http(s):// in the list of wikis. Just the domain name.
+# eg. gardening.wikia.com is OK, but http://gardening.wikia.com is not.
 
 ## Variables declaration ##
 wiki_index_file=$1
@@ -10,13 +12,19 @@ wikis=`cat $wiki_index_file`
 for wiki in $wikis; do
 	echo "Downloading wiki dump for $wiki..."
 	perl get_pages.pl $wiki
-	if [ $? -eq 0 ]; then
+	if [ $? -eq 0 ]; then 	
 		echo "Joining all data parts into one xml file for $wiki..."
 		sh join_xml_parts.sh $wiki
 		rm -r data/processed/$wiki/parts
 		python3 ../wiki_dump_parser/wiki_dump_parser.py data/processed/$wiki/full/$wiki.xml
-		rm data/processed/$wiki/full/$wiki.xml
-	else
+		if [ $? -eq 0 ]; then   
+			rm data/processed/$wiki/full/$wiki.xml
+		else    # error control for parser 
+			echo "Error when processing dump for $wiki. Skipping..."
+			echo $wiki >> skipped.txt
+			rm data/$wiki*.xml
+		fi
+	else	# error control for downloader
 		echo "Error when downloading dump for $wiki. Skipping..."
 		echo $wiki >> skipped.txt
 		rm data/$wiki*.xml

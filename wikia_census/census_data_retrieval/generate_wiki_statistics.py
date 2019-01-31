41a52e3ce5850761727c1634c090d3339c31be27
Abel Serrano Juste <akronix5@gmail.com>
2019-01-31 09:18:11 +0100

    Small improvements in format and code comments

--- wikia_census/census_data_retrieval/generate_wiki_statistics.py
+++ wikia_census/census_data_retrieval/generate_wiki_statistics.py
@@ -3,14 +3,16 @@ Extract the main stats of the wikis in Wikia requesting the Wikia API (http://ww
 The requests use the url and the ByString endpoint and it returns a JSON object
 with the stats of the wiki. Some URLs in the Wikia Index fail when requesting the API.
 
-After gathering API stats, the JSON objects are flattened (some stats are prefixed with "stats." 
-because they are inside a "stats" object in the original JSON object) and some 
-information (like the description, information related with images and top users) 
-is removed. Additionally, the number of nonarticles is computed using 
+After gathering API stats, the JSON objects are flattened (some stats are prefixed with "stats."
+because they are inside a "stats" object in the original JSON object) and some
+information (like the description, information related with images and top users)
+is removed. Additionally, the number of nonarticles is computed using
 stats.articles (the number of content pages) and stats.pages (the total number of pages contained in the wiki)
 
 Finally, the stats are stored in a CSV file with the following columns:
+- 'creation_date'
 - 'domain'
+- 'founding_user_id'
 - 'headline'
 - 'hub'
 - 'id'
@@ -37,7 +39,7 @@ import requests
 import json
 import time
 from pandas.io.json import json_normalize
-    
+
 url = 'http://www.wikia.com/api/v1/Wikis/ByString?expand=1&limit=25&batch=1&includeDomain=true&string='
 
 def requestStats(link):
@@ -45,7 +47,7 @@ def requestStats(link):
 
     Keyword arguments:
     link -- url of the wiki involved in the request
-    
+
     Return:
     An object with the stats of the wiki or None
     """
@@ -67,7 +69,7 @@ i=0
 output_filename = "wikia_stats"
 
 # Read the wikia index file
-with open('../data/20180917-curatedIndex.txt') as f:
+with open('../data/20190125-curatedIndex.txt') as f:
     links = [line.strip() for line in f]
 
 # Repeat for every URL (it takes about 1500ms per query, so take it easy)
@@ -76,6 +78,7 @@ for link in links:
     if (data is None):
         print ("Error processing link: {} ({})".format(i,link))
     else:
+        #~ print(data)
         wikia.append(data)
         i+=1
 
@@ -84,10 +87,10 @@ for link in links:
 df = json_normalize(wikia)
 
 # Remove unnecessary columns
-df.drop(columns=['desc','flags', 'image', 'original_dimensions.height','original_dimensions.width', 'topUsers', 'wordmark'], inplace=True)
+df.drop(columns=['desc','flags', 'image', 'original_dimensions.height','original_dimensions.width', 'topUsers', 'wordmark'], inplace=True) #TOBECHANGED by the columns we actually want
 
-# Compute nonarticles 
-df['stats.nonarticles'] = df['stats.pages']-df['stats.articles']   
+# Compute nonarticles
+df['stats.nonarticles'] = df['stats.pages']-df['stats.articles']
 
 # Save to CSV
 timestr = time.strftime("%Y%m%d")
